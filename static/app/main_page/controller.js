2170629e99b84654aca3503390400d645054b971
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2017-09-22 11:07:12 -0300

    Google chart -> Angular google chart

--- static/app/main_page/controller.js
+++ static/app/main_page/controller.js
@@ -16,4 +16,41 @@ angular.module('app').controller('controller',
     }
   });
 
+  
+   $scope.myChartObject = {};
+    
+    $scope.myChartObject.type = "BarChart";
+    
+    $scope.onions = [
+        {v: "Onions"},
+        {v: 3},
+    ];
+
+    $scope.myChartObject.data = {"cols": [
+        {id: "t", label: "Topping", type: "string"},
+        {id: "s", label: "Slices", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "Mushrooms"},
+            {v: 3},
+        ]},
+        {c: $scope.onions},
+        {c: [
+            {v: "Olives"},
+            {v: 31}
+        ]},
+        {c: [
+            {v: "Zucchini"},
+            {v: 1},
+        ]},
+        {c: [
+            {v: "Pepperoni"},
+            {v: 2},
+        ]}
+    ]};
+
+    $scope.myChartObject.options = {
+        'title': 'How Much Pizza I Ate Last Night'
+    }; 
+
 }]);
